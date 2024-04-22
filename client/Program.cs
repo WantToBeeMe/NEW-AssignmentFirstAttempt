ede3cc0529b26f09f2d10e0f2d0dcd1e0f7a9481
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-04-22 12:32:27 +0200

    p1, set up connection, send helle message back and forth

--- client/Program.cs
+++ client/Program.cs
@@ -10,7 +10,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("client trying to start");
         ClientUDP cUDP = new ClientUDP();
         cUDP.start();
     }
@@ -18,19 +17,74 @@ class Program
 
 class ClientUDP
 {
-
     //TODO: implement all necessary logic to create sockets and handle incoming messages
+    //TODO: create all needed objects for your sockets 
+    private const int PORT = 32000; //port of the server
+    private const string SERVER_IP = "127.0.0.1";
+    private const int THRESHOLD = 20;
+    private const int BUFFER_SIZE = 1024;
+    private const int TIMEOUT_MS = 1000;
+    
+    private Socket socket;
+    private IPEndPoint serverEndPoint;
+    private int slowStartThreshold;
+    private int currentWindowSize;
+    private int nextExpectedMessageId;
+    private Dictionary<int, string> receivedData;
+    private List<int> unacknowledgedMessageIds;
+    
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
     public void start()
-    {
-
+    {   //DIRK: for some reason this methods name is uncapitalized, just leave it as is
+        //DIRK: any comment not written with DIRK or ISSAM as prefix is not written by us and should not be removed
+        
+        slowStartThreshold = THRESHOLD;
+        currentWindowSize = 1;
+        nextExpectedMessageId = 1;
+        receivedData = new Dictionary<int, string>();
+        unacknowledgedMessageIds = new List<int>();
+        
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        serverEndPoint = new IPEndPoint(IPAddress.Parse(SERVER_IP), PORT);
+        
+        SendHelloMessage();
+        ReceiveWelcomeMessage();
     }
-    //TODO: create all needed objects for your sockets 
 
     //TODO: [Send Hello message]
-
+    private void SendHelloMessage()
+    {
+        Message helloMessage = new Message { Type = MessageType.Hello, Content = slowStartThreshold.ToString() };
+        byte[] data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(helloMessage));
+        socket.SendTo(data, serverEndPoint);
+        Console.WriteLine("Sent Hello message to server.");
+    }
+    
     //TODO: [Receive Welcome]
+    private void ReceiveWelcomeMessage()
+    {
+        EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        byte[] data = new byte[BUFFER_SIZE];
+        int bytesReceived = socket.ReceiveFrom(data, ref remoteEndPoint);
+        Message? message = JsonSerializer.Deserialize<Message>(
+            Encoding.UTF8.GetString(data, 0, bytesReceived)
+            );
+        if (message == null)
+        {
+            HandleError("Failed to deserialize message.");
+            return;
+        }
+        if (message.Type != MessageType.Welcome)
+        {
+            HandleError($"Expected Welcome message, but received {message.Type}");
+            return;
+        }
 
+        Console.WriteLine("Received Welcome message from server.");
+    }
+    
+    
+    
     //TODO: [Send RequestData]
 
     //TODO: [Receive Data]
@@ -40,6 +94,9 @@ class ClientUDP
     //TODO: [Send End]
 
     //TODO: [Handle Errors]
-
-
+    private void HandleError(string errorMessage)
+    {
+        Console.WriteLine($"Error: {errorMessage}");
+        // DIRK: this handle error method is not complete!!
+    }
 }
--- server/Program.cs
+++ server/Program.cs
@@ -13,7 +13,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("server trying to start");
         ServerUDP sUDP = new ServerUDP();
         sUDP.start();
     }
@@ -21,23 +20,98 @@ class Program
 
 class ServerUDP
 {
-
     //TODO: implement all necessary logic to create sockets and handle incoming messages
+    //TODO: create all needed objects for your sockets 
+    private const int PORT = 32000; // port of the server
+    private const int BUFFER_SIZE = 1024;
+    private const int DATA_CHUNK_SIZE = 100;
+    private const int TIMEOUT_MS = 1000;
+    
+    private Socket socket;
+    private IPEndPoint clientEndPoint;
+    private int slowStartThreshold;
+    private int currentWindowSize;
+    private int nextDataMessageId;
+    
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
     public void start()
-    {
-
+    {   //DIRK: for some reason this methods name is uncapitalized, just leave it as is
+        //DIRK: any comment not written with DIRK or ISSAM as prefix is not written by us and should not be removed
+        
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        socket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+        
+        //DIRK: investigate if we are allowed to use CancellationTokenSource. and even if we should use it
+        //      now it just does this forever which is good enough for now, but obviously we should not do this in the real submission
+        while (true)
+        {
+            RecieveClientMessage();
+        }
     }
-
-    //TODO: create all needed objects for your sockets 
 
     //TODO: keep receiving messages from clients
     // you can call a dedicated method to handle each received type of messages
+    private void RecieveClientMessage()
+    {
+        EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        byte[] data = new byte[BUFFER_SIZE];
+        int bytesReceived = socket.ReceiveFrom(data, ref remoteEndPoint);
+        Message? message = JsonSerializer.Deserialize<Message>(
+            Encoding.UTF8.GetString(data, 0, bytesReceived)
+            );
+        if (message == null)
+        {
+            Console.WriteLine("Failed to deserialize message.");
+            return;
+            //DIRK: Investigate what we should do if the message failst to deserialize, aka in this code block
+            //      should we then just ignore this message? at least we should let the server know right, or maybe send error back to cleint?
+            //      i think any error should be reported to the client right? 
+        }
+        switch (message.Type)
+        {
+            case MessageType.Hello:
+                HandleHelloMessage(message, remoteEndPoint);
+                break;
+            case MessageType.RequestData:
+                Console.WriteLine("Received RequestData");
+                break;
+            case MessageType.Ack:
+                Console.WriteLine("Received Ack");
+                break;
+            case MessageType.Error:
+                Console.WriteLine("Received Error");
+                break;
+            default:
+                Console.WriteLine("Received unknown message type");
+                break;
+        }
+    }
 
     //TODO: [Receive Hello]
-
+    private void HandleHelloMessage(Message message, EndPoint remoteEndPoint)
+    {
+        Console.WriteLine("Received Hello message from client.");
+        Console.WriteLine(message);
+        clientEndPoint = (IPEndPoint)remoteEndPoint;
+        if (!int.TryParse(message.Content, out slowStartThreshold))
+        {
+            Console.WriteLine("Invalid slow start threshold");
+            //DIRK: Investigate what we should do if there is no threshold given, aka in this code block
+            //      should we then just ignore this message? or set a default? or something else? 
+        }
+        currentWindowSize = 1;
+        nextDataMessageId = 1;
+   
+        SendWelcomeMessage();
+    }
     //TODO: [Send Welcome]
-
+    private void SendWelcomeMessage()
+    {
+        Message welcomeMessage = new Message { Type = MessageType.Welcome };
+        byte[] data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(welcomeMessage));
+        socket.SendTo(data, clientEndPoint);
+    }
+    
     //TODO: [Receive RequestData]
 
     //TODO: [Send Data]
@@ -49,6 +123,4 @@ class ServerUDP
     //TODO: [Handle Errors]
 
     //TODO: create all needed methods to handle incoming messages
-
-
 }
